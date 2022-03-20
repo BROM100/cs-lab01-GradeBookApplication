a8dd96b9914a50b77afdde1c9131efc670a25e40
Bartek Antos <bartekantos@protonmail.com>
2022-03-20 21:53:19 +0100

    CalculateStatistics method added

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -4,9 +4,9 @@ using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    public class RankedGradeBook :BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
-        public RankedGradeBook(string name): base(name)
+        public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
         }
@@ -14,7 +14,7 @@ namespace GradeBook.GradeBooks
         public override char GetLetterGrade(double averageGrade)
         {
             if (Students.Count < 5)
-                throw new InvalidOperationException("Not enough students");
+                throw new InvalidOperationException("The number of students must be greater that 5.");
             else if (averageGrade >= 80)
                 return 'A';
             else if (averageGrade >= 60)
@@ -26,6 +26,7 @@ namespace GradeBook.GradeBooks
             else
                 return 'F';
         }
+
         public override void CalculateStatistics()
         {
             if (Students.Count < 5)
@@ -34,9 +35,19 @@ namespace GradeBook.GradeBooks
                 base.CalculateStatistics();
         }
 
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students");
+            else
+                base.CalculateStudentStatistics(name);
+
+
+        }
 
     }
-
-
-
 }
+
+
+
+
